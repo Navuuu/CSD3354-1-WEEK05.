89df7fdc867231eb75e57dc5ed5c1c6efb1ef3e7
visualstudio <visualstudio@122-PC30>
2019-02-08 18:21:01 -0500

    6:20

--- Week5/Program.cs
+++ Week5/Program.cs
@@ -9,8 +9,34 @@ namespace Week5
     class Program
     {
         static void Main(string[] args)
+
         {
-            int i = 9;
+            elevator e = new elevator();
+            e.run();
+        }
+        class Node
+        {
+            public Node() { }
+            public Node floor;
+            public Node elevatorUp;
+            public string FloorNumber;
+        }
+        class elevator
+        {
+            Node FirstFloor;
+            Node SecondFloor;
+            Node ThirdFloor;
+            Node FourthFloor;
+
+            public void run()
+            {
+                FirstFloor = new Node();
+                FirstFloor.FloorNumber = "First Floor";
+                Console.WriteLine("floor number is {0}", FirstFloor.FloorNumber);
+                FirstFloor.elevatorUp = SecondFloor;
+                SecondFloor.FloorNumber = "Second Floor";
+                Console.WriteLine("floor number is {0}", SecondFloor.FloorNumber);
+            }
         }
     }
 }
